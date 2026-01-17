5f4e47022446245d52bf185e88ea126dd82869fa
isaacw123434 <isaacmwilloughby@gmail.com>
2026-01-16 22:50:33 -0500

    Delete test_timezone.py

--- test_timezone.py
+++
@@ -1,9 +0,0 @@
-from datetime import datetime
-from zoneinfo import ZoneInfo
-import os
-
-print("Current System Time (What scraper currently uses):")
-print(datetime.now().strftime("%Y-%m-%d %H:%M:%S"))
-
-print("\nPeru Time (Goal):")
-print(datetime.now(ZoneInfo("America/Lima")).strftime("%Y-%m-%d %H:%M:%S"))
